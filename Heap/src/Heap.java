06004166fc56e384f857ed0708b32361b52293a2
Shelby Huston <shelby.huston@rocky.edu>
2016-11-04 08:45:26 -0600

    kys

--- Heap/src/Heap.java
+++ Heap/src/Heap.java
@@ -1,4 +1,30 @@
 
-public class Heap {
+public class Heap<E> implements RMCPriorityQueue<E>{
+
+	@Override
+	public E peak() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public  E poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void add(E elt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 
 }
---
+++ Heap/src/RMCPriorityQueue.java
@@ -0,0 +1,7 @@
+
+public interface RMCPriorityQueue<E> {
+	public E peak ();
+	public E poll();
+	public void add(E elt);
+	public int size();
+}
